12b42d51b1f1d6e68675c018fc96efe95ef91e0a
Tarnjot Singh <tarnjot.singh.505@Gmail.com>
2025-07-06 21:53:24 +0100

    PDO database connection set up.

---
+++ includes/db.php
@@ -0,0 +1,12 @@
+<?php
+
+// Path to my SQLite database file
+
+$db_file = __DIR__ . '/../expense-tracker.db';
+
+try {
+    $pdo = new PDO("sqlite:" . $db_file);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
